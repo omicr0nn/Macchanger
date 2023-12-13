9670770621a9d0065f3cf2b1715b3219ada0f5e0
omicron <131537767+omicr0nn@users.noreply.github.com>
2023-12-13 22:00:20 +0300

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# coding by omicr0n
+# Coding by omicr0n
 import os
 import sys
 import time
 from os import system
 
 system("cls||clear")
+
 def rainbow_text(text, delay=0.1):
     colors = [
-    "\033[31m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m", "\033[37m",
+        "\033[31m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m", "\033[37m",
     ]
 
     for i in range(len(text)):
@@ -21,39 +22,38 @@ def rainbow_text(text, delay=0.1):
 
     sys.stdout.write("\033[0m")
     sys.stdout.write("\n")
-	
+
 if __name__ == "__main__":
     rainbow_text("Coding by Omicron")
-	    
-print("""
-\033[35mMAC Adres Değiştirme Programına Hoş Geldiniz / omicr0n \033[0m
 
-\033[32m 1) MAC Adresi Random Belirle\033[0m
-\033[31m 2) MAC Adresi Elle Belirle\033[0m
-\033[94m 3) MAC Adresi Orijinale Döndür\033[0m
+print("""
+\033[35mWelcome to the MAC Address Changer Program / omicr0n \033[0m
+
+\033[32m 1) Set MAC Address to Random\033[0m
+\033[31m 2) Set MAC Address Manually\033[0m
+\033[94m 3) Restore Original MAC Address\033[0m
 """)
 
-islemno = input("İşlem No Girin: ")
+operation_no = input("Enter Operation No: ")
 
-if(islemno=="1"):
-	os.system("ifconfig eth0 down")
-	os.system("macchanger -r eth0")
-	os.system("ifconfig eth0 up")
-	print("\033[32m Yeni MAC Adresi Random Belirlendi. \033[0m")
+if operation_no == "1":
+    os.system("ifconfig eth0 down")
+    os.system("macchanger -r eth0")
+    os.system("ifconfig eth0 up")
+    print("\033[32m New MAC Address Set to Random. \033[0m")
 
+elif operation_no == "2":
+    mac_address = input("\033[31m Enter New MAC Address: ")
+    os.system("ifconfig eth0 down")
+    os.system("macchanger --mac " + mac_address + " eth0")
+    os.system("ifconfig eth0 up")
+    print("\033[31m New MAC Address Set Manually. \033[0m")
 
-elif(islemno=="2"):
-	macadres = input("\033[31m Yeni MAC Adres Girin: ")
-	os.system("ifconfig eth0 down")
-	os.system("macchanger --mac " + macadres + " eth0")
-	os.system("ifconfig eth0 up")
-	print("\033[31m Yeni MAC Adresi Elle Belirlendi. \033[0m")
-
-elif(islemno=="3"):
-	os.system("ifconfig eth0 down")
-	os.system("macchanger -p eth0")
-	os.system("ifconfig eth0 up")
-	print("\033[94m MAC Adresi Orijinale Döndürüldü. \033[0m")
+elif operation_no == "3":
+    os.system("ifconfig eth0 down")
+    os.system("macchanger -p eth0")
+    os.system("ifconfig eth0 up")
+    print("\033[94m MAC Address Restored to Original. \033[0m")
 
 else:
-	print("Hata! Kali linux kullansana kardeşim")
+    print("Error! Please use Kali Linux.")
